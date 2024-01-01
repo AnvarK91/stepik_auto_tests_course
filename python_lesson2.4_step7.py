a4da7193d849438596505c924bdf91398ace014d
Anvar K <anvar-k_91@mail.ru>
2024-01-01 06:08:07 +0600

    Add .py file lesn2.4_step7

---
+++ python_lesson2.4_step7.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    txt = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+    button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "book")))
+    button.click()
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "span#input_value")
+    x = x_element.text
+    y = calc(x)
+    inpt = browser.find_element(By.CSS_SELECTOR, "input#answer")
+    inpt.send_keys(y)
+    button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "solve")))
+    button.click()
+    # assert "successful" in message.text
+
+finally:
+
+    time.sleep(10)
+    browser.quit()
